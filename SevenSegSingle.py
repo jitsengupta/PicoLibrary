451290af2b0a2fa8e768cd615720669352a07620
Jit Sengupta <jitsengupta@users.noreply.github.com>
2023-05-26 13:17:24 -0400

    Added SevenSegSingle.py
    Created a simple SevenSegSingle class to display a number in a single seven segment display, either directly connected via GPIO or through a shift register.

---
+++ SevenSegSingle.py
@@ -0,0 +1,93 @@
+"""
+# SevenSegSingle.py
+# A simple implementation of a single seven segment display
+# unit. Raw unit connected either directly through GPIO pins
+# or using a shift register.
+# To create the raw unit, send A=n, B=n, C=n... E=n as parameters
+# To create the shift register version, send
+# dataPin, clockPin and latchPin nos
+#
+# See a demo at: https://wokwi.com/projects/365454777517844481
+# Author: Arijit Sengupta
+"""
+
+
+from machine import Pin
+import random
+import utime
+import time
+
+class SevenSegSingle:
+"""
+SevenSegSingle class - implementation of a single seven-segment display
+Can be connected directly via GPIO or via a shift register
+
+Currently only displays numbers. Also only supports common cathode.
+Will create a common anode version in a future version
+"""
+
+  def __init__(self, A=0, B=1, C=2, D=3, E=4, F=5, G=6, *, dataPin=None, clockPin=None, latchPin=None):
+  """
+  Constructor - call as SevenSeg(A=n, B=n, C=n... G=n) for direct connection
+  or as  SevenSeg(dataPin=n, clockPin=n, latchPin=n) if using a shift register
+  """
+  
+    pinarray = [A, B, C, D, E, F, G]
+    self._digitcodes = ["11111100", "01100000", "11011010", "11110010", "01100110",
+                          "10110110", "10111110", "11100000", "11111110", "11110110"]
+
+    self._parallelPins = []
+    if dataPin is None:
+      for p in range(0, 7):
+        self._parallelPins.append(Pin(pinarray[p],Pin.OUT))
+      self._data = None
+    else:
+      self._data = Pin(dataPin, Pin.OUT)
+      self._latch = Pin(latchPin, Pin.OUT)
+      self._clock = Pin(clockPin, Pin.OUT)
+
+  def show(self, n):
+  """
+  Show a number on the seven segment display - only supports 0-9
+  """
+  
+    if n < 0 or n > 9:
+      raise ValueError('This class can only display a number between 0-9')
+    else:
+      if self._data is None:
+        self._parallel_update(self._digitcodes[n])
+      else:
+        self._shift_update(self._digitcodes[n])
+
+  def _parallel_update(self, input):
+    for i in range(0,7):
+      self._parallelPins[i].value(int(input[i])) 
+
+  def _shift_update(self, input):
+    #put latch down to start data sending
+    self._clock.value(0)
+    self._latch.value(0)
+    self._clock.value(1)
+    
+    #load data in reverse order
+    for i in range(7, -1, -1):
+      self._clock.value(0)
+      self._data.value(int(input[i]))
+      self._clock.value(1)
+
+    #put latch up to store data on register
+    self._clock.value(0)
+    self._latch.value(1)
+    self._clock.value(1)
+
+### Test this module if needed. Direct connect uses the default
+### A=0 through G=7
+        
+if __name__ == '__main__':
+  d1 = SevenSegSingle(dataPin = 16, clockPin = 18, latchPin = 17)
+  d2 = SevenSegSingle()
+  for i in range(0,100000):
+    d1.show(i%10)
+    d2.show(i%10)
+    time.sleep(0.3)
+
