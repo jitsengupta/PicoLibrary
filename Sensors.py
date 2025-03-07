75a0ec71013e434c0f05231c3b714a069aeed0b9
Jit Sengupta <amdroid007@gmail.com>
2025-03-07 15:40:18 -0500

    Updated Sensors with new design and MPU implementation
    Updated the Sensors hierarchy with new design based on latest Sensor class hierarchy. Also added MPU6050 implementation.

--- Sensors.py
+++ Sensors.py
@@ -10,6 +10,7 @@ from machine import Pin, ADC
 import utime
 import math
 import dht
+from mpu6050 import *
 from Log import *
 
 class Sensor:
@@ -20,20 +21,25 @@ class Sensor:
     
     Parameters
     --------
-    pin: pin number where the sensor is connected
     lowActive: set to True if the sensor gets low (or under threshold)
     when tripped. So an analog light sensor should normally get a high
     value but when covered, go low. So lowActive should be True
     
     A force sensor would be opposite - tripped when force gets high
-    so lowActive should be False
+    so lowActive should be False.
+
+    Some of the digital sensors such as flame sensors, proximity sensors
+    are lowActive, while others such as PIR sensors are highActive. Please
+    check the sensor documentation for the correct value.
     """
     
-    def __init__(self, pin, name='Sensor', lowActive = True):
-        self._pin = pin
+    def __init__(self, name='Sensor', lowActive = True):
         self._lowActive = lowActive
         self._name = name
-        
+
+    def rawValue(self):
+        Log.e(f"rawValue not implemented for {type(self).__name__} {self._name}")
+
     def tripped(self)->bool:
         Log.e(f"tripped not implemented for {type(self).__name__} {self._name}")
         return False
@@ -43,15 +49,24 @@ class DigitalSensor(Sensor):
     A simple digital sensor (like the commonly available LC-393 that is a light sensor)
     has a digital output that flips based on a manual threshold control
 
-    We are just going to poll this to keep things simple
+    We are just going to poll this to keep things simple.
+
+    Parameters
+    --------
+    pin: the pin number to which the sensor is connected
+    name: the name of the sensor
+    lowActive: set to True if the sensor gets low when tripped.
     """
     
     def __init__(self, pin, name='Digital Sensor', lowActive=True):
-        super().__init__(pin, name, lowActive)
-        self._pinio = Pin(self._pin, Pin.IN)
+        super().__init__(name, lowActive)
+        self._pinio = Pin(pin, Pin.IN)
 
+    def rawValue(self):
+        return self._pinio.value()
+    
     def tripped(self)->bool:
-        v = self._pinio.value()
+        v = self.rawValue()
         if (self._lowActive and v == 0) or (not self._lowActive and v == 1):
             Log.i(f"DigitalSensor {self._name}: sensor tripped")
             return True
@@ -63,6 +78,9 @@ class TiltSensor(DigitalSensor):
     A tilt sensor looks like a cap but has just a metal ball on two contacts
     when you tilt, the ball rolls off and the contact opens. That way its very
     much like a button which is always pressed.
+
+    Connect the Tilt sensor across an IO pin and GND. When the sensor is tripped
+    the pin will go high.
     """
     
     def __init__(self, pin, name='Tilt Sensor'):
@@ -71,7 +89,12 @@ class TiltSensor(DigitalSensor):
         self._pinio = Pin(pin, Pin.IN, Pin.PULL_UP)
         
     def tripped(self):
-        if self._pinio.value() == 1:
+        """
+        A tilt sensor connector is typically always closed, so we
+        initialize its pin using the internal pullup resistor. It is
+        tripped when the value goes high, so there it is never lowActive
+        """
+        if self.rawValue() == 1:
             Log.i(f"TiltSensor {self._name}: sensor tripped")
             return True
         else:
@@ -88,8 +111,8 @@ class AnalogSensor(Sensor):
     def __init__(self, pin, name='Analog Sensor', lowActive=True, threshold = 30000):
         """ analog sensors will need to be sent a threshold value to detect trip """
         
-        super().__init__(pin, name, lowActive)
-        self._pinio = ADC(self._pin)
+        super().__init__(name, lowActive)
+        self._pinio = ADC(pin)
         self._threshold = threshold
 
     def tripped(self)->bool:
@@ -105,34 +128,62 @@ class AnalogSensor(Sensor):
     def rawValue(self):
         return self._pinio.read_u16()
 
-class TempSensor(AnalogSensor):
+class TemperatureSensor():
     """
-    Temperature sensor as a subclass of Analog Sensor - Temp sensor is a bit more
-    defined since it technically returns a temperature. Note that thermistors are
-    not totally accurate, but the teperature fluctuations - increases and decreases
-    are what matters.
+    TemperatureSensor is essentially an abstract class/interface
+    Implementing here as a basic class without using any special Python 3 notations
     """
     
-    def __init__(self, pin, name='Temp Sensor', lowActive=False, threshold=60):
+    def temperature(self, unit='C'):
+        """
+        Return the temperature in the appropriate unit. Let's only support
+        degrees Celcius (C) and Fahrenheit (F).
+        """
+        Log.e(f"temperature not implemented for {type(self).__name__} {self._name}")
+        
+
+    def _celciusToFahrenheit(self, t):
+        return t*1.8 + 32
+
+
+class Thermistor(AnalogSensor, TemperatureSensor):
+    """
+    Thermistor as a subclass of Analog Sensor - and it "implements" the
+    TemperatureSensor interface. The rawValue is the resistance of the thermistor
+    which is then converted to temperature using the Steinhart-Hart equation.
+    """
+    
+    def __init__(self, pin, name='Thermistor', lowActive=False, threshold=30):
         """
         Create a new temp sensor - similar to regular analog sensor
         but now tripped will return true when temp is lower than threshold (lowActive=True)
         and higher than threshold (lowActive=False)
+
+        tempthreshold is in degrees Celcius
         """
         
-        super().__init__(pin, name, lowActive, threshold)
+        AnalogSensor.__init__(self, pin, name, lowActive, threshold)
         
     def rawValue(self):
         """
-        Reture the temperature (approx) in fahrenheit
+        Reture the temperature (approx) in celsius
         """
         
-        adcvalue = super().rawValue()
+        adcvalue = AnalogSensor.rawValue(self)
         voltage = adcvalue / 65535.0 * 5
         Rt = 10 * voltage / (5 -voltage)
         tempK = (1 / (1 / (273.15+25) + (math.log(Rt/10)) / 3950))
-        tempF = int((tempK - 273.15) * 1.8) + 32
-        return tempF
+        tempC = tempK - 273.15
+        return tempC
+    
+    def temperature(self, unit='C'):
+        if unit == 'C':
+            return self.rawValue()
+        elif unit == 'F':
+            return self._celciusToFahrenheit(self.rawValue())
+        else:
+            Log.e(f"Unknown unit {unit} for temperature")
+            return None  
         
 class UltrasonicSensor(Sensor):
     """
@@ -152,12 +203,16 @@ class UltrasonicSensor(Sensor):
     """
 
     def __init__(self, *, trigger=0, echo=1, name='Ultrasonic', lowActive = True, threshold=10.0):
-        super().__init__(trigger, name, lowActive)
+        super().__init__(name, lowActive)
         self._trigger = Pin(trigger, Pin.OUT)
-        self._echo = Pin(echo, Pin.IN)
+        self._echo = Pin(echo, Pin.IN, Pin.PULL_DOWN)
         self._threshold = threshold
 
-    def getDistance(self)->float:
+    def rawValue(self):
+        """ Return the distance in cm """
+        return self.distance()
+    
+    def distance(self)->float:
         """ Get the distance of obstacle from the sensor in cm """
         
         self._trigger.off()
@@ -176,7 +231,7 @@ class UltrasonicSensor(Sensor):
     def tripped(self)->bool:
         """ sensor is tripped if distance is higher or lower than threshold """
         
-        v = self.getDistance()
+        v = self.rawValue()
         if (self._lowActive and v < self._threshold) or (not self._lowActive and v > self._threshold):
             Log.i(f"UltrasonicSensor {self._name}: sensor tripped")
             return True
@@ -184,8 +239,8 @@ class UltrasonicSensor(Sensor):
             return False
 
 # DHT11/DHT22 Sensor
-class DHTSensor(DigitalSensor):
-    def __init__(self, pin, name='DHT', lowActive=False, threshold=60, poll_delay=2000, sensor_type='DHT11'):
+class DHTSensor(DigitalSensor, TemperatureSensor):
+    def __init__(self, pin, name='DHT', lowActive=False, threshold=30, poll_delay=2000, sensor_type='DHT11'):
         """
         Create a new DHT sensor - similar to regular digital sensor but can take
         either the form of a DHT11 or DHT22 based on the sensor_type parameter
@@ -198,19 +253,19 @@ class DHTSensor(DigitalSensor):
         Also, the DHT sensor is a bit slow, so we will not poll it as frequently as other sensors.
         To avoid to much polling, a default poll parameter is set to 2 seconds.
 
-        The threshold is set to 60F by default, but can be changed. This is used to determine
+        The threshold is set to 30 deg C by default, but can be changed. This is used to determine
         if the sensor is tripped or not. Only the temperature is used for tripping.
         """
         
-        super().__init__(pin, name, lowActive)
+        DigitalSensor.__init__(self,pin, name, lowActive)
         self._sensor_type = sensor_type
         self._sensor_class = dht.DHT11 if sensor_type == "DHT11" else dht.DHT22
-        self._dht_sensor = self._sensor_class(Pin(self._pin))
+        self._dht_sensor = self._sensor_class(Pin(pin))
         self._last_poll_time = 0
         self._poll_delay = poll_delay
         self._threshold = threshold
 
-    def getTemperature(self):
+    def temperature(self, unit='C'):
         """
         Return the temperature of the sensor
         """
@@ -218,9 +273,17 @@ class DHTSensor(DigitalSensor):
         if utime.ticks_ms() - self._last_poll_time > self._poll_delay:
             self._dht_sensor.measure()
         self._last_poll_time = utime.ticks_ms()
-        return self._dht_sensor.temperature()
+        t = self._dht_sensor.temperature()
 
-    def getHumidity(self):
+        if unit == 'C':
+            return t
+        elif unit == 'F':
+            return self._celciusToFahrenheit(t)
+        else:    
+            Log.e(f"Unknown unit {unit} for temperature")
+            return None      
+
+    def humidity(self):
         """
         Return the humidity of the sensor
         """
@@ -246,6 +309,95 @@ class DHTSensor(DigitalSensor):
         """
         
         if self._lowActive:
-            return self.getTemperature() < self._threshold
+            return self.temperature() < self._threshold
         else:
-            return self.getTemperature() >= self._threshold
+            return self.temperature() >= self._threshold
+        
+
+# MPU6050 Sensor
+class MPU(Sensor, TemperatureSensor):
+    """
+    The MPU Sensor is a 6-axis sensor that returns acceleration and gyro data
+    It is a digital sensor that uses I2C to communicate with the Pico. It is
+    not an analog sensor, but it is a continuous sensor, so we subclass Sensor
+    instead of DigitalSensor.
+
+    I am using an MPU6050 driver that auto-calibrates the sensor when the class
+    is initialized. Ensure that the sensor is placed on a flat surface when the
+    class is initialized. If this cannot be guaranteed, call the calibrate method
+    when possible to re-initialize the sensor. The calibration offsets are printed
+    to the console when calibration is complete. Once calibration data is available,
+    it may be passed as an argument to the constructor to avoid recalibration.
+
+    The MPU6050 sensor is a 3.3V sensor, so ensure that the vcc pin of the sensor
+    is connected to the 3.3V pin of the Pico. The sensor is connected to the I2C bus
+    of the Pico, so ensure that the SDA and SCL pins are connected correctly.
+    """
+
+    def __init__(self, name='MPU6050', sda = 0, scl = 1, ofs=None, lowActive=False, threshold=30):
+        """
+        Parameters:
+        name: the name of the sensor
+        sda, scl = the SDA and SCL pins of the I2C bus
+        ofs = the calibration offsets of the sensor (if available)
+        """
+
+        Sensor.__init__(self, name, lowActive)
+        self._i2cid = -1 # lets set an invalid value to start with
+        self._sda = sda
+        self._scl = scl
+
+        if (sda == 0 and scl == 1) or (sda == 4 and scl == 5) or (sda == 8 and scl == 9) or (sda == 12 and scl == 13) or (sda == 16 and scl == 17) or (sda == 20 and scl == 21):    
+            self._i2cid = 0
+        elif (sda == 2 and scl == 3) or (sda == 6 and scl == 7) or (sda == 10 and scl == 11) or (sda == 14 and scl == 15) or (sda == 18 and scl == 19) or (sda == 26 and scl == 27):
+            self._i2cid = 1
+        else:
+            raise ValueError('Invalid SDA/SCL pins')
+
+        self._mpu = MPU6050(self._i2cid, sda, scl, ofs)
+
+    def calibrate(self):
+        """
+        Calibrate the sensor by placing it on a flat surface
+        re-initialize the sensor which will auto-calibrate
+        """
+
+        self._mpu = MPU6050(self._i2cid, self._sda, self._scl)
+
+    def temperature(self, unit='C'):
+        """
+        Return the temperature of the sensor
+        """
+
+        if unit == 'C':
+            return self._mpu.celsius
+        elif unit == 'F':
+            return self._mpu.fahrenheit
+        else:
+            Log.e(f"Unknown unit {unit} for temperature")
+            return None
+        
+    def rawValue(self):
+        """
+        Return the raw data from the sensor
+        which is in the form of a named tuple containing both acceleration and gyro data
+        """
+
+        return self._mpu.data
+    
+    def angles(self):
+        """
+        Return the angles of the sensor in the form of a named tuple containing the pitch and roll angles
+        """
+
+        return self._mpu.angles
+    
+    def tripped(self)->bool:
+        """
+        Sensor is tripped if temperature is higher or lower than threshold
+        """
+
+        if self._lowActive:
+            return self.temperature() < self._threshold
+        else:
+            return self.temperature() >= self._threshold
