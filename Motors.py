de13c59862ba8a1b9531b02d5668ef3b689868a3
Jit Sengupta <amdroid007@gmail.com>
2025-06-09 15:15:01 -0400

    DC Motor implemention with a 1-pin direction control
    ES8266 motor shield has a motor driver with a single speed control PWM pin and a single direction control (instead of two separate direction control pins). This update allows DCMotor to have only a forward_pin (backward_pin defaults to None) so sending 1 to forward_pin moves motor forward, and 0 moves backward. backward_pin myst be None for this logic to work.

--- Motors.py
+++ Motors.py
@@ -196,32 +196,44 @@ class CoolingFan(Motor):
 class DCMotor(CoolingFan):
     """
     Implements a standard DC motor with 2 pins that can be controlled
-    for bidirectional controllable velocities.
+    for bidirectional controllable velocities. Typically the enable_pin is used for
+    PWM control of the speed, and the forward_pin and backward_pin are used for
+    controlling the direction of the motor.
+
+    Direction can also be controlled using a single pin by using the enable_pin
+    for PWM and setting the forward_pin to 1 or 0 to control direction. In this case,
+    use None for backward_pin.
+
     Needs to use a driver chip like L293N or L298N to function correctly.
     """
     
-    def __init__(self, enable_pin = 0, name="DCMotor", forward_pin = 1, backward_pin=2, min_duty=15000, max_duty=65535):
+    def __init__(self, enable_pin = 0, name="DCMotor", forward_pin = 1, backward_pin=None, min_duty=15000, max_duty=65535):
         super().__init__(enable_pin, name, min_duty, max_duty)
         self.pin1 = Pin(forward_pin, Pin.OUT)
-        self.pin2 = Pin(backward_pin, Pin.OUT)
+        if backward_pin is None:
+            self.pin2 = None
+        else:
+            self.pin2 = Pin(backward_pin, Pin.OUT)
 
     def forward(self, speed=100):
         """ Spin motor forward at a percent speed (max:100)"""
         
         Log.i(f"Moving {self._name} forward at speed {speed}")
         self.speed = speed
-        self.enable_pin.duty_u16(self.duty_cycle(self.speed))
         self.pin1.value(1)
-        self.pin2.value(0)
+        if self.pin2 is not None:
+            self.pin2.value(0)
+        self.enable_pin.duty_u16(self.duty_cycle(self.speed))
 
     def backwards(self, speed=100):
         """ Spin motor backwards at a percent speed (max:100) """
         
         Log.i(f"Moving {self._name} backwards at speed {speed}")
         self.speed = speed
-        self.enable_pin.duty_u16(self.duty_cycle(self.speed))
         self.pin1.value(0)
-        self.pin2.value(1)
+        if self.pin2 is not None:
+            self.pin2.value(1)
+        self.enable_pin.duty_u16(self.duty_cycle(self.speed))
 
     def stop(self):
         """ Stop spinning the motor """
@@ -229,7 +241,8 @@ class DCMotor(CoolingFan):
         Log.i(f"Stopping {self._name}")
         self.enable_pin.duty_u16(0)
         self.pin1.value(0)
-        self.pin2.value(0)
+        if self.pin2 is not None:
+            self.pin2.value(0)
 
 if __name__ == '__main__':
     m = DCMotor(enable_pin=13, forward_pin=14,backward_pin=15)
